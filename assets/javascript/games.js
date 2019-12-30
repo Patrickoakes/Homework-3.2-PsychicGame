79832809b6b33d4394aa003fd487ab392204411b
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-30 07:02:13 -0800

    commit

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,1 +1,10 @@
-//console.log('hello');
+var comChoice = Math.floor(Math.random() * 25 + 1);
+            var Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', ' k', 'l', 'm', ' n', ' o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            var i = i < Letters.length;
+           
+           
+            function psychicFunction(comChoice, Letters) {
+                var comChoice = Math.floor(Math.random() * 25 + 1);
+                console.log(Letters[comChoice]);
+            }
+            console.log(Letters[comChoice]);
