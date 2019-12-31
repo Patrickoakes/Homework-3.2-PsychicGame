29431c5fc584e78620b3b395dae8052168280938
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-31 15:23:13 -0800

    Commit Final Homework 3

---
+++ assets/javascript/games-corrected.js
@@ -0,0 +1,121 @@
+ //var userGuess = e.key//   NO USE 
+            //var letters = ["a", "b", "c"];  Solution version    
+            var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', ' k', 'l', 'm', ' n', ' o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+            // This array will hold what we guess
+            // var guessedLetters = [];  Solution version
+            var guessesArr = ['', '', '', '', '', '', '', '', ''];
+            // This variable will be randomly assigned one of the three letters
+            //var letterToGuess = null; Solution Version
+            var letIndex = 0; //Function 2
+            // var comChoice = Math.floor(Math.random() * 25 + 1);  NOT USED 
+            // This is what we'll use to count down///nothing
+            var guessesLeft = 9;
+
+            // This is the counter for wins/losses//  Same as Solution
+            var wins = 0;
+            var losses = 0;
+
+
+            ////////////////////////////////////// Turn var anything function () {}
+            //these turned into  functions////
+            // var winContainer = document.querySelector("winCount");
+            // var lossContainer = document.querySelector("#lossCount");
+            // var guessContainer = document.querySelector("#guessLeft");// Function 1
+            // var log = document.getElementById('guessCount'); // FUnction 2
+            //////////////////////////////////////
+            // Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
+            // var updateGuessesLeft = function () {//SOLUTION
+            var guessContainer = function () {
+                // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
+                // (i.e. guessesLeft will get displayed in HTML)
+                document.querySelector("#guessleft").innerHTML = guessesLeft;
+            };
+            /////////var updateLetterToGuess = function () { ///////////////////////////////////////////////
+            var log = function () {
+
+                var updateLetIndex = function psychicFunction(comChoice, letters) {
+                    var comChoice = Math.floor(Math.random() * 25 + 1);
+                    console.log(letters[comChoice]);
+                }
+                console.log(letters[comChoice]);
+                // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
+                ///  //letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+            };
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            document.addEventListener('keyup', logKey);
+            function logKey(e) {
+                log.textContent += `${e.key}`;
+            }
+
+            // Function will be called when we reset everything/// NOT IN MINE AT ALL
+            var reset = function () {
+                guessesLeft = 9;
+                guessesArr = [];
+                updateLetIndex();
+                guessContainer();
+                updateGuessesSoFar();
+            };
+
+            ///MAIN PROCESS
+            updateLetIndex()   ///updateWin();
+            guessContainer();  //updateLoss()
+
+            // This function will capture the keyboard clicks.
+            document.onkeydown = function (event) {
+                // It's going to reduce the guesses by one
+                guessesLeft--;
+
+                // Lowercase the letter
+                var letter = event.key.toLowerCase();
+
+                // Then add the guess to the guessedLetters array
+                guessesArr.push(letter);
+
+                //Update Function
+                updateLogKey();
+                updateGuessesSoFar();
+
+                /////////////////////////////////////////////////////////////////////////////////////////////
+                // We'll check if there's a match.
+                if (letter === letIndex) {
+
+                    // If there is then we win and we'll update the HTML to display the win.
+                    wins++;
+                    document.querySelector("#wins").innerHTML = wins;
+
+                    // Then we'll reset the game
+                    reset();
+                }
+
+                // If we are out of guesses...
+                if (guessesLeft === 0) {
+
+                    // Then we will loss and we'll update the HTML to display the loss.
+                    losses++;
+                    document.querySelector("#losses").innerHTML = losses;
+
+                    // Then we'll call the reset.
+                    reset();
+                }
+            };
+
+            //    for (guessesIndex = 0; guessesIndex < guessesArr.length; guessesIndex++) {
+                //     if (userGuess === letters[letIndex]) {
+                //         log.textContent += `${e.key}`;
+                //     } else (guessesIndex === guessesArr); {
+                //         function updateLoss() {
+                //             lossContainer.innerHTML = losses; losses++; updateLoss();
+                //         }
+     
+                //     }
+                // };
+     
+            ///////////////////////////////////////////////////////////////////
+                //  function updateWin() {
+                //   if (userGuess === comChoice)
+                //         winContainer.innerHTML = wins; wins++; updateWin();
+                //   }
+               // function updateWin() {
+                //     if (userGuess === comChoice)
+                //         winContainer.innerHTML = wins; wins++; updateWin();
+                // }   -->
--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,16 +1,80 @@
+var userGuess = e.key
+
+
+var wins = 0;
+
+
+var losses = 0;
+
+
 var comChoice = Math.floor(Math.random() * 25 + 1);
-            var Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', ' k', 'l', 'm', ' n', ' o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-            var i = i < Letters.length;
-           
-           
-            function psychicFunction(comChoice, Letters) {
-                var comChoice = Math.floor(Math.random() * 25 + 1);
-                console.log(Letters[comChoice]);
-            }
-            console.log(Letters[comChoice]);
 
-            document.addEventListener('keyup', logKey);
-            function logKey(e) {
 
-                log.textContent += `${e.key}`;
-            }
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', ' k', 'l', 'm', ' n', ' o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var letIndex = 0;
+
+var guessesArr = ['', '', '', '', '', '', '', '', ''];
+////////////////////////////////////// Turn var anything function () {}
+//these went inside functions////
+var winContainer = document.querySelector("winCount");
+var lossContainer = document.querySelector("#lossCount");
+var guessContainer = document.querySelector("#guessLeft");
+const log = document.getElementById('guessCount');
+//////////////////////////////////////
+// for (guessesIndex = 0; guessesIndex < guessesArr.length; guessesIndex++) {
+if (guessesArr[guessesIndex] === letters[letIndex]) {
+} else (guessesIndex === guessesArr); {
+    function updateLoss() {
+        lossContainer.innerHTML = losses; losses++; updateLoss();
+    }
+}
+
+function updateWin() {
+    if (userGuess === comChoice)
+        winContainer.innerHTML = wins; wins++; updateWin();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///MAIN PROCESS
+updateWin();
+updateLoss();
+
+document.addEventListener('keyup', logKey);
+function logKey(e) {
+    log.textContent += `${e.key}`;
+}
+
+for (guessesIndex = 0; guessesIndex < guessesArr.length; guessesIndex++) {
+    if (userGuess === letters[letIndex]) {
+        log.textContent += `${e.key}`;
+    } else (guessesIndex === guessesArr); {
+        function updateLoss() {
+            lossContainer.innerHTML = losses; losses++; updateLoss();
+        }
+
+    }
+};
+//////////////////////////////////////////////////////////////////////////
+
+function updateWin() {
+    if (userGuess === comChoice)
+        winContainer.innerHTML = wins; wins++; updateWin();
+}
+
+function psychicFunction(comChoice, letters) {
+    var comChoice = Math.floor(Math.random() * 25 + 1);
+    console.log(letters[comChoice]);
+}
+console.log(letters[comChoice]);
